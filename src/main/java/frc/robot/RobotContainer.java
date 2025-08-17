d170cf1f58346186e5467b8f7c61f3438beebb40
ACsNotThere <aaethancheung@gmail.com>
2025-08-16 22:18:58 -0400

    Added Swerve to RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,9 +4,15 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
+import java.io.File;
+
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.subsystems.SwerveSubsystem;
+import swervelib.SwerveInputStream;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -19,10 +25,15 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
+  // TODO: Initialize your DriveSubsystem here...
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController = new CommandXboxController(
+  public final static CommandXboxController m_driverController = new CommandXboxController(
       OperatorConstants.kDriverControllerPort);
+
+  public final SwerveSubsystem m_swerveSubsystem = new SwerveSubsystem(
+      m_driverController,
+      new File(Filesystem.getDeployDirectory(), "swerve"));
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -31,6 +42,19 @@ public class RobotContainer {
     // Configure the trigger bindings
     configureBindings();
   }
+
+  public SwerveInputStream driveAngularVelocity = SwerveInputStream
+      .of(m_swerveSubsystem.getSwerveDrive(), () -> m_driverController.getLeftY() * -1,
+          () -> m_driverController.getLeftX() * -1)
+      .withControllerRotationAxis(() -> m_driverController.getRightX() * -1)
+      .deadband(OperatorConstants.DEADBAND).scaleTranslation(0.8)
+      .allianceRelativeControl(true);
+
+  public SwerveInputStream driveRobotOriented = driveAngularVelocity.copy().robotRelative(true)
+      .allianceRelativeControl(false);
+
+  Command driveFieldOrientedAnglularVelocity = m_swerveSubsystem.driveFieldOriented(driveAngularVelocity);
+  Command driveRobotOrientedAngularVelocity = m_swerveSubsystem.driveFieldOriented(driveRobotOriented);
 
   /**
    * Use this method to define your trigger->command mappings. Triggers can be
@@ -47,6 +71,9 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
+    // m_driveSubsystem.setDefaultCommand(new ArcadeDriveCommand(m_driveSubsystem,
+    // m_driverController));
+    m_swerveSubsystem.setDefaultCommand(driveRobotOrientedAngularVelocity);
   }
 
   /**
@@ -54,6 +81,7 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  // public Command getAutonomousCommand() {
-  // }
+  public Command getAutonomousCommand() {
+    return null;
+  }
 }
