770b04e21578a56ebc97a9f488782a0fc846225d
Pix3ldog <Kate.Kag3@gmail.com>
2025-08-28 23:05:32 -0400

    kinda done
     think I'm done the algae and coral subsystem besides                                                                                                     the ids and hopefully it works and does not blow up the robot

--- src/main/java/frc/robot/subsystems/AlgaeCoralIndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/AlgaeCoralIndexerSubsystem.java
@@ -4,14 +4,61 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.AlgaeCoralIndexerConstants;
 
 public class AlgaeCoralIndexerSubsystem extends SubsystemBase {
   /** Creates a new AlgaeCoralIndexerSubsystem. */
-  public AlgaeCoralIndexerSubsystem() {}
+  private final TalonFX m_motor;
+  private final DigitalInput m_kewlbeambreak ;
+  private final DigitalInput m_kewlbeambreak2 ;
+  /*instsmth motors and beam breaks */
+  public AlgaeCoralIndexerSubsystem() {
+    m_motor = new TalonFX(AlgaeCoralIndexerConstants.kitkat) ; 
+    /*kitkats r yummy */
+    m_kewlbeambreak = new DigitalInput(AlgaeCoralIndexerConstants.canoe) ;
+    /*idk what to wrie here  */
+    m_kewlbeambreak2 = new DigitalInput(AlgaeCoralIndexerConstants.spiderman) ;
+    /*spiderman is secretly James Bond */
+    
+  }
 
-  @Override
+
+  private boolean hasAlgae (){
+    return !m_kewlbeambreak.get();
+  }
+  private boolean hasCoral () {
+    return !m_kewlbeambreak2.get() ;
+  }
+  /*check if bakon has not stolen coral */
+/*hi */
+  private void voltagecontrol(double voltorb){
+  m_motor.setVoltage(voltorb);
+  
+
+
+  }
+  public double getIndexerVoltage() {
+    return m_motor.getMotorVoltage().getValue();
+}
+  public void algaeAndCoralOrNo() {
+    if (hasCoral()) {
+      if (hasAlgae()) {
+        voltagecontrol(AlgaeCoralIndexerConstants.algaeRemovalVoltage);
+      } else {
+        voltagecontrol(0);
+      }
+    } else {
+      voltagecontrol(AlgaeCoralIndexerConstants.coralReleaseVoltage); }
+    }
+
+  
+    @Override
   public void periodic() {
+    algaeAndCoralOrNo();
     // This method will be called once per scheduler run
   }
 }
